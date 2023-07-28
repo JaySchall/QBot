8a21b3b64a6b352551540dbedd6d4590496dc268
Jonathan Schall <62366496+JaySchall@users.noreply.github.com>
2023-07-27 20:24:19 -0400

    Start to setting up raid stuff

--- bot.py
+++ bot.py
@@ -6,8 +6,23 @@ from discord import app_commands
 from dotenv import load_dotenv
 import settings
 
+embeds = {}
 priority = []
+queue = []
+priorityUsers = []
 
+async def onRaid(message):
+    patreonsPulled = 0
+    s = message.content + "\n"
+    for i in range(0, 3):
+        if patreonsPulled < patreonsPulled and len(priorityUsers) > 0:
+            guy = priorityUsers.pop(0)
+            queue.remove(guy)
+            await guy.send(message.content)
+        elif len(queue) > 0:
+            guy = queue.pop(0)
+            await guy.send(message.content)
+        
 def run_bot():
     intents = discord.Intents.default()
     client = discord.Client(intents=intents)
@@ -20,6 +35,12 @@ def run_bot():
 
         synced = await tree.sync()
         await interaction.response.send_message(f"Synced {len(synced)} commands")
+
+    @tree.command(name="createEmbed", description="creates an embed used to join and leave the queue")
+    @app_commands.checks.has_permissions(administrator = True)
+    async def sync(interaction: discord.Interaction):
+        channel = interaction.channel
+        print()
 
     @tree.error
     async def on_app_command_error(interaction, error):
