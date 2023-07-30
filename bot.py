c022c878ed4526e71f617a26acd429583fcc1e22
Jonathan Schall <62366496+JaySchall@users.noreply.github.com>
2023-07-30 19:18:09 -0400

    Setup embeds and buttons

--- bot.py
+++ bot.py
@@ -6,10 +6,41 @@ from discord import app_commands
 from dotenv import load_dotenv
 import settings
 
-embeds = {}
+embed = discord.Embed(title="Queue")
+embed.add_field(name="Users in Queue", value="1.")
+embedMessages = {}
 priority = []
 queue = []
 priorityUsers = []
+
+class JoinButton(discord.ui.View):
+     def __init__(self):
+         super().__init__()
+         self.timeout=None
+     @discord.ui.button(label='Join Queue', style=discord.ButtonStyle.green)
+     async def join(self, interaction: discord.Interaction, button: discord.ui.Button):
+        user = interaction.user
+        if user in queue:
+            await interaction.response.send_message("You are already in queue", ephemeral=True)
+            return
+        queue.append(user)
+        print(f"{user.mention} joined queue")
+        await interaction.response.send_message("You have joined the queue", ephemeral=True)
+         
+
+     @discord.ui.button(label='Leave Queue', style=discord.ButtonStyle.green)
+     async def leave(self, interaction: discord.Interaction, button: discord.ui.Button):
+        user = interaction.user
+        if user not in queue:
+            await interaction.response.send_message("You already not in queue", ephemeral=True)
+            return
+        queue.remove(user)
+        print(f"{user.mention} left queue")
+        await interaction.response.send_message("You have left the queue", ephemeral=True)
+
+async def updateEmbeds():
+    for id in embedMessages:
+        print("get message idk")
 
 async def onRaid(message):
     patreonsPulled = 0
@@ -24,10 +55,16 @@ async def onRaid(message):
             await guy.send(message.content)
         
 def run_bot():
-    intents = discord.Intents.default()
+    intents = discord.Intents.all()
     client = discord.Client(intents=intents)
     tree = app_commands.CommandTree(client)
-    client.run(settings.TOKEN)
+    
+
+    @client.event
+    async def on_ready():
+        print("HAIII")
+        #synced = await tree.sync()
+        #print(f"Synced {len(synced)} commands")
 
     @tree.command(name="sync", description="sync commands")
     @app_commands.checks.has_permissions(administrator = True)
@@ -36,11 +73,16 @@ def run_bot():
         synced = await tree.sync()
         await interaction.response.send_message(f"Synced {len(synced)} commands")
 
-    @tree.command(name="createEmbed", description="creates an embed used to join and leave the queue")
+    @tree.command(name="createembed", description="creates an embed used to join and leave the queue")
     @app_commands.checks.has_permissions(administrator = True)
     async def sync(interaction: discord.Interaction):
         channel = interaction.channel
-        print()
+        if channel in embedMessages:
+            await interaction.response.send_message(f"Channel already has an embed")
+            return
+        msg = await channel.send(embed=embed, view=JoinButton())
+        embedMessages[channel.id] = msg.id
+        await interaction.response.send_message(f"Created embed")
 
     @tree.error
     async def on_app_command_error(interaction, error):
@@ -50,8 +92,11 @@ def run_bot():
     @client.event
     async def on_message(message):
         if client.get_channel(settings.queueChannel) == message.channel:
+            print(message.content)
             if settings.raidString in message.content:
-                print("")
+                await onRaid(message)
+                print("HI OMG SOMETHING HAPPENED")
 
+    client.run(settings.TOKEN)
 if __name__ == "__main__":
     run_bot()
--- settings.py
+++ settings.py
@@ -4,9 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-queueServer = os.getenv('QUEUE_SERVER')
-queueChannel = os.getenv('QUEUE_CHANNEL')
-loggingChannel = os.getenv('LOGGING_CHANNEL')
+queueServer = int(os.getenv('QUEUE_SERVER'))
+queueChannel = int(os.getenv('QUEUE_CHANNEL'))
+loggingChannel = int(os.getenv('LOGGING_CHANNEL'))
 
 raidString = os.getenv('RAID_CODE_IDENTIFIER')
-queueName = ""
+#queueName = ""
