b085d95f18e648bc4b92c54d70cf45a963e1c2b0
Jonathan Schall <62366496+JaySchall@users.noreply.github.com>
2023-08-02 18:36:38 -0400

    Updating and viewing embeds

--- bot.py
+++ bot.py
@@ -14,9 +14,10 @@ queue = []
 priorityUsers = []
 
 class JoinButton(discord.ui.View):
-     def __init__(self):
+     def __init__(self, client):
          super().__init__()
          self.timeout=None
+         self.client = client
      @discord.ui.button(label='Join Queue', style=discord.ButtonStyle.green)
      async def join(self, interaction: discord.Interaction, button: discord.ui.Button):
         user = interaction.user
@@ -25,6 +26,7 @@ class JoinButton(discord.ui.View):
             return
         queue.append(user)
         print(f"{user.mention} joined queue")
+        await updateEmbeds(self.client)
         await interaction.response.send_message("You have joined the queue", ephemeral=True)
          
 
@@ -36,13 +38,25 @@ class JoinButton(discord.ui.View):
             return
         queue.remove(user)
         print(f"{user.mention} left queue")
+        await updateEmbeds(self.client)
         await interaction.response.send_message("You have left the queue", ephemeral=True)
 
-async def updateEmbeds():
+async def updateEmbeds(client):
+    embed.remove_field(index=0)
+    s = ""
+    i = 1
+    for dude in queue:
+        
+        s += str(i) + ". " +  f"<@{dude.id}>" + "\n"
+        i+=1
+    embed.add_field(name="Users in Queue", value=s)
     for id in embedMessages:
-        print("get message idk")
+        channel = client.get_channel(id)
+        msg = await channel.fetch_message(embedMessages[id])
+        await msg.edit(embed=embed)
+        print("an attempt")
 
-async def onRaid(message):
+async def onRaid(message, client):
     patreonsPulled = 0
     s = message.content + "\n"
     for i in range(0, 3):
@@ -53,6 +67,7 @@ async def onRaid(message):
         elif len(queue) > 0:
             guy = queue.pop(0)
             await guy.send(message.content)
+    await updateEmbeds(client)
         
 def run_bot():
     intents = discord.Intents.all()
@@ -80,9 +95,19 @@ def run_bot():
         if channel in embedMessages:
             await interaction.response.send_message(f"Channel already has an embed")
             return
-        msg = await channel.send(embed=embed, view=JoinButton())
+        msg = await channel.send(embed=embed, view=JoinButton(client))
         embedMessages[channel.id] = msg.id
         await interaction.response.send_message(f"Created embed")
+
+    @tree.command(name="viewembeds", description="see where the embeds are located")
+    @app_commands.checks.has_permissions(administrator = True)
+    async def viewembeds(interaction: discord.Interaction):
+        s = ""
+        for id in embedMessages:
+            channel = client.get_channel(id)
+            msg = await channel.fetch_message(embedMessages[id])
+            s+=channel.name+":\n" + msg.jump_url + "\n\n"
+        await interaction.response.send_message(s)
 
     @tree.error
     async def on_app_command_error(interaction, error):
@@ -94,7 +119,7 @@ def run_bot():
         if client.get_channel(settings.queueChannel) == message.channel:
             print(message.content)
             if settings.raidString in message.content:
-                await onRaid(message)
+                await onRaid(message, client)
                 print("HI OMG SOMETHING HAPPENED")
 
     client.run(settings.TOKEN)
